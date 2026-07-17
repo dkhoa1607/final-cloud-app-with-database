38a08c7a0230ecef0b744dacca6b7b25a30845dd
dkhoa1607 <90122387+dkhoa1607@users.noreply.github.com>
2026-07-17 17:30:53 +0700

    Name the OnlineCourse admin section

--- onlinecourse/apps.py
+++ onlinecourse/apps.py
@@ -2,4 +2,6 @@ from django.apps import AppConfig
 
 
 class OnlinecourseConfig(AppConfig):
+    default_auto_field = 'django.db.models.AutoField'
     name = 'onlinecourse'
+    verbose_name = 'OnlineCourse'
